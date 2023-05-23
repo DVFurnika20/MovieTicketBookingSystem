35977b8d8d6cbf44ed4dda739a6764bd15365c69
DVFurnika20 <DVFurnika20@codingburgas.bg>
2023-05-23 09:46:59 +0300

    Start project anew with Raylib

--- App/MagiCinema/main.cpp
+++ App/MagiCinema/main.cpp
@@ -1,14 +1,44 @@
-#include <iostream>
-#include <conio.h>
-#include <windows.h>
-#include "menu.h"
+#include "raylib.h"
 
-using namespace std;
-
-int main()
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
 {
-    Menu menu({ "Option 1", "Option 2", "Exit" });
-    menu.run();
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
